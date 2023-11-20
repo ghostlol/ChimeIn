29fa081602f9f8a05189d418473b973265731f4c
Harshit Chaudhary <hc4638873@gmail.com>
2023-11-20 18:12:00 +0530

    day1

---
+++ frontend/src/Pages/Chatpage.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const Chatpage = () => {
+    const [chats, setstate] = useState([])
+    const fetchChats = async() => {
+    const {data} = await axios.get('/api/chat');
+    setstate(data);
+    };
+    useEffect(() => {
+        fetchChats();
+    },[]);
+  
+    return <div> {chats.map((chat) => (<div key={chat._id}>{chat.chatName}</div>))} </div>;
+  
+}
+
+export default Chatpage
