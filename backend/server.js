7dffcb30906003d0e32eb61234adfcf3d5ac96d7
Harshit Chaudhary <hc4638873@gmail.com>
2023-11-22 17:50:50 +0530

    database setup

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const dotenv = require('dotenv')
-const { chats } = require("./data/data")
-const app = express();
+const connectDB = require('./config/db');
+const colors = require("colors");
+const userRoutes = require("./routes/userRoutes"); 
+
 dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(express.json()); // To accept json data from frontend
+app.use("/api/user", userRoutes);
 
 app.get('/', (req, res)=>{
     res.send("Api is running");
 });
 
-app.get('/api/chat', (req, res)=>{
-    res.send(chats);
-});
+// app.get('/api/chat', (req, res)=>{
+//     res.send(chats);
+// });
 
-app.get('/api/chat/:id', (req, res)=>{
-    const singleChat = chats.find((c)=>c._id === req.params.id);
-    res.send(singleChat);
-});
+// app.get('/api/chat/:id', (req, res)=>{
+//     const singleChat = chats.find((c)=>c._id === req.params.id);
+//     res.send(singleChat);
+// });
 
 const PORT = process.env.PORT || 4000
-app.listen(PORT,console.log(`Server started on PORT ${PORT}`));
+app.listen(PORT,console.log(`Server started on PORT ${PORT}`.yellow.bold));
