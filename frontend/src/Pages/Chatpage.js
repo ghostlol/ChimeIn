ef875ed7e63d65ad012ce0debb1753fcc31e1a8f
Harshit Chaudhary <hc4638873@gmail.com>
2023-11-30 18:42:04 +0530

    chat ui

--- frontend/src/Pages/Chatpage.js
+++ frontend/src/Pages/Chatpage.js
@@ -1,18 +1,22 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import { Box } from "@chakra-ui/react";
+import { ChatState } from "../Context/ChatProvider";
+import SideDrawer from "../components/miscellaneous/SideDrawer";
+import MyChats from "../components/MyChats";
+import ChatBox from "../components/ChatBox";
 
 const Chatpage = () => {
-    const [chats, setstate] = useState([])
-    const fetchChats = async() => {
-    const {data} = await axios.get('/api/chat');
-    setstate(data);
-    };
-    useEffect(() => {
-        fetchChats();
-    },[]);
-  
-    return <div> {chats.map((chat) => (<div key={chat._id}>{chat.chatName}</div>))} </div>;
-  
+
+    const {user} = ChatState();
+    return (<div style={{width: "100%"}}> 
+        {user && <SideDrawer/>}
+        <Box display='flex' justifyContent='space-between' width='100%' height='91.5vh'p='10px'>
+            {user && <MyChats/>}
+            {user && <ChatBox/>}
+
+
+        </Box>
+
+    </div>);
 }
 
-export default Chatpage
+export default Chatpage;
